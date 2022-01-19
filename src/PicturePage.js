c607374967c4a115ac06ec9047e2fe11b4d3dc4d
Suzanne Lee <suzannebhlee@gmail.com>
2022-01-18 20:21:39 -0500

    add error page

--- src/PicturePage.js
+++ src/PicturePage.js
@@ -1,13 +1,14 @@
 import { Card } from "@shopify/polaris";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function PicturePage() {
   const apiKey = process.env.REACT_APP_API_KEY;
   const params = useParams();
   let [mediaItem, setMediaItem] = useState([]);
+  let [hasError, setHasError] = useState(false);
 
   let url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${params.date}&thumbs=true`;
 
@@ -19,27 +20,14 @@ export default function PicturePage() {
   }
 
   function onError(e) {
-    console.error("e", Object.keys(e));
-    console.error("e", e.response);
+    // console.error("e", Object.keys(e));
+    // console.error("e", e.response);
+
+    setHasError(true);
   }
 
-  useEffect(() => {
-    axios.get(url).then(fxn, onError);
-  }, [url]);
-
-  return (
-    <main style={{ padding: "1rem 0" }}>
-      <Link
-        to="/"
-        style={{
-          display: "block",
-          marginLeft: "10%",
-          marginTop: "5%",
-          marginBottom: "3%",
-        }}
-      >
-        &lt;&lt; Spacestagram Main Page
-      </Link>
+  const mediaCard = !hasError ? (
+    <React.Fragment>
       <Card>
         <div className="text-container">
           <h1>{mediaItem.title}</h1>
@@ -59,6 +47,39 @@ export default function PicturePage() {
         src={src}
         alt={mediaItem.title}
       />
+    </React.Fragment>
+  ) : null;
+
+  const errorCard = hasError ? (
+    <Card>
+      <div className="text-container">
+        <h1>Oops!</h1>
+        <div className="description">
+          We couldn't load the image for the given date.
+        </div>
+      </div>
+    </Card>
+  ) : null;
+
+  useEffect(() => {
+    axios.get(url).then(fxn, onError);
+  }, [url]);
+
+  return (
+    <main style={{ padding: "1rem 0" }}>
+      <Link
+        to="/"
+        style={{
+          display: "block",
+          marginLeft: "10%",
+          marginTop: "5%",
+          marginBottom: "3%",
+        }}
+      >
+        &lt;&lt; Spacestagram Main Page
+      </Link>
+      {mediaCard}
+      {errorCard}
     </main>
   );
 }
